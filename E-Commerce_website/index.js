035579ade10068b49103be998c062d3d90cb09f2
Nevyin32 <allthought123@gmail.com>
2024-01-10 22:16:39 -0800

    40% completed

---
+++ E-Commerce_website/index.js
@@ -0,0 +1,10 @@
+//Side-Navbar
+var sidebar = document.querySelector(".Nav-Menu")
+
+function opennavbar(){
+    sidebar.style.left = "0"
+}
+
+function closenavbar(){
+    sidebar.style.left = "-60%"
+}
